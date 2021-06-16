a9ad3edf05f868e5a11edecffe17847c3f1b5760
ANGELA <angela.mbithe@student.moringaschool.com>
2021-06-16 13:22:52 +0300

    add authentication forms

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
-from . import db
+from . import db,login_manager
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 class User(UserMixin, db.Model):
@@ -12,6 +16,17 @@ class User(UserMixin, db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
 
     def __repr__(self):
@@ -28,6 +43,18 @@ class Admin (UserMixin, db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
 
 
     def __repr__(self):
