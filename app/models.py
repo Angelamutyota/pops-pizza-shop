3ffa9f003d326a3c5bb42a65e6702c1e535e2ea6
ANGELA <angela.mbithe@student.moringaschool.com>
2021-06-16 09:00:51 +0300

    remove secret key

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from . import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
--- start.sh
+++
@@ -1,3 +0,0 @@
-export SECRET_KEY='ghad71yp'
-
-python3.8 manage.py server
