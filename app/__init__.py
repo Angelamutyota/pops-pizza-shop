f7c7f10eddfc98c50ad546bf228e5dbe3c196879
ANGELA <angela.mbithe@student.moringaschool.com>
2021-06-16 12:54:12 +0300

    add user and admin class

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 
@@ -16,7 +15,6 @@ mail = Mail()
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
@@ -27,7 +25,6 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
      # configure UploadSet
-    configure_uploads(app,photos)
 
 
     # Initializing flask extensions
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -9,3 +11,40 @@ class User(UserMixin, db.Model):
     secure_password = db.Column(db.String(255),nullable = False)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+class Admin (UserMixin, db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    admin = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+
