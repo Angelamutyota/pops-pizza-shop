4a83f8861c9ce4a6ba5a95a902f15b9d87473d3e
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-06-17 19:05:02 +0300

    Adding database for toppings and pizza

--- app/models.py
+++ app/models.py
@@ -82,4 +82,97 @@ class Role(db.Model):
         return f'User {self.name}'
 
 
+class Orders(db.Model):
 
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    price_id = db.Column(db.currency)
+    pizza_type = db.Column(db.String)
+    image_path = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_order(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reviews(cls, id):
+        orders = Orders.query.filter_by(price_id=id).all()
+        return orders
+
+
+class Orders:
+
+    all_orders = []
+
+    def __init__(self, price_id, title, imageurl, order):
+        self.price_id = price_id
+        self.title = title
+        self.imageurl = imageurl
+        self.order = order
+
+    def save_order(self):
+        Orders.all_orders.append(self)
+
+    @classmethod
+    def clear_orders(cls):
+        Orders.all_orders.clear()
+
+    @classmethod
+    def get_orders(cls, id):
+
+        response = []
+
+        for order in cls.all_orders:
+            if order.order_id == id:
+                response.append(order)
+
+        return response
+
+
+class Toppings(db.Model):
+
+    __tablename__ = 'toppings'
+    id = db.Column(db.Integer, primary_key=True)
+    topping_id = db.Column(db.currency)
+    topping_type = db.Column(db.String)
+    image_path = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_order(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_topping(cls, id):
+        orders = Toppings.query.filter_by(topping_id=id).all()
+        return orders
+
+
+class Toppings:
+
+    all_orders = []
+
+    def __init__(self, topping_id, title, imageurl, order):
+        self.topping_id = topping_id
+        self.title = title
+        self.imageurl = imageurl
+        self.order = order
+
+    def save_order(self):
+        Toppings.all_orders.append(self)
+
+    @classmethod
+    def clear_orders(cls):
+        Toppings.all_orders.clear()
+
+    @classmethod
+    def get_orders(cls, id):
+
+        response = []
+
+        for order in cls.all_orders:
+            if order.topping_id == id:
+                response.append(order)
+
+        return response
