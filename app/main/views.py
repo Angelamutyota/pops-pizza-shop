ddb7ed017a3a0e4d403a227402a815a2eec6bfa6
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-06-17 10:52:51 +0300

    reqular changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from . import main 
 from flask import render_template, request, redirect, url_for, abort
-
+from ..models import Order 
+from flask_login import login_required, current_user 
 
 @main.route('/')
 def index():
@@ -21,3 +22,35 @@ def index():
     #     return redirect(url_for('main.search', pizza_name=pizza))
     # else:
     return render_template('index.html', title=title)
+
+
+@main.route('/order/<int:id>')
+@login_required
+def orders (id):
+    orders = Order.query.filter_by(id=id).first()
+    
+    if orders is None: 
+        abort (404)
+        
+#adding a comment
+
+
+# @main.route('/display_orders/<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def display_order(id):
+#     ''' function to post comments '''
+#     form = CommentForm()
+#     title = 'post comment'
+#     pitches = Pitch.query.filter_by(id=id).first()
+
+#     if pitches is None:
+#         abort(404)
+
+#     if form.validate_on_submit():
+#         opinion = form.opinion.data
+#         new_comment = Comments(
+#             opinion=opinion, user_id=current_user.id, pitches_id=pitches.id)
+#         new_comment.save_comment()
+#         return redirect(url_for('.view_pitch', id=pitches.id))
+
+#     return render_template('post_comment.html', comment_form=form, title=title)
